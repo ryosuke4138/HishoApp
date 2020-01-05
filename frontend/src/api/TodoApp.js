4ff7c1fc0cfa2b84e68e982607846f2ca3bf8fe2
ryosuke4138 <ryoryo4138@gmail.com>
2020-01-05 19:05:26 +0900

    [WIP]

--- frontend/src/api/TodoApp.js
+++ frontend/src/api/TodoApp.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'http://localhost:3003'; // json-server
+// const API_BASE_URL = 'http://127.0.0.1:3003'; // json-server
 const API_BASE_URL = 'http://127.0.0.1:8000'; // Django
 
 const client = axios.create({
