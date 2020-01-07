54ea5c70351c08d984d976e84183cc711f794ee3
ryosuke4138 <ryoryo4138@gmail.com>
2020-01-07 21:24:49 +0900

    [add] time to model

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 
 
+# def CATEGORY():
+#     return (('a', 'A'), ('b', 'B'), ('c', 'C'))
+
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    status = models.CharField(default='Unstarted', max_length=100)
+    status = models.CharField(
+        default='Unstarted',
+        max_length=100
+    )
+    # category = models.IntegerField(
+    #     blank=True,
+    #     choices=CATEGORY
+    # )
+    created_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        auto_now_add = True
+    )
+    deadline = models.DateTimeField(
+        blank=True,
+        null=True
+    )
+    completed_at = models.DateTimeField(
+        blank = True,
+        null = True
+    )
 
     def __str__(self):
         """A string representation of the model."""
