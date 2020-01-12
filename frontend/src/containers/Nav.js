5f156c407ea4f1fba0a97011ce3c2838ea716186
ryosuke4138 <ryoryo4138@gmail.com>
2020-01-12 21:32:49 +0900

    [WIP]

---
+++ frontend/src/containers/Nav.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import Nav from '../components/Nav'
+
+const mapStateToProps = state => ({
+  // state.shopping.categoriesをprops.categoriesに紐付け
+  categories: state
+})
+
+// react-router-reduxのpushでページ遷移を発生させる
+const mapDispatchToProps = dispatch => ({
+  onClick (path) {
+    dispatch(push(path))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
